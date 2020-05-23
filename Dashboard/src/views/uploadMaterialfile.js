c90401a3daf0f538cb3f01a05e533eafde47599f
Vasundhra91 <51117751+Vasundhra91@users.noreply.github.com>
2020-05-24 04:19:07 +0530

    ZzZz

--- Dashboard/src/views/uploadMaterialfile.js
+++ Dashboard/src/views/uploadMaterialfile.js
@@ -4,7 +4,7 @@ import { userContext } from 'views/Logincontext';
 import axios from "axios";
 import Select from "react-select";
 class uploadfile extends Component {
- constructor(props) {
+  constructor(props) {
     super(props);
     this.onFileChange = this.onFileChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -12,44 +12,44 @@ class uploadfile extends Component {
     this.state = {
       profileImg: '',
       profileImg_data: [],
-      dataname:'',
+      dataname: '',
       visiblebutton: true,
-        selectedOption: "",
-        selectedlabel:"",
-        data: { label: "Loading ...", value: "" },
-        loading: true,
+      selectedOption: "",
+      selectedlabel: "",
+      data: { label: "Loading ...", value: "" },
+      loading: true,
     }
   }
   handleChange = selectedOption => {
     this.setState({ selectedOption: selectedOption.value });
     this.setState({ selectedlabel: selectedOption.label });
-};
+  };
   static contextType = userContext;
   componentDidMount() {
     const { user } = this.context
-        let obj = user;
-        let keys = Object.keys(obj);
-        let lat = obj[keys[0]].Userdetails;
-      
-        if (lat.UserAdmin === "N") {
-          this.setState({ visiblebutton: false })
-        }
+    let obj = user;
+    let keys = Object.keys(obj);
+    let lat = obj[keys[0]].Userdetails;
+
+    if (lat.UserAdmin === "N") {
+      this.setState({ visiblebutton: false })
+    }
     fetch('/uploadfile/files')
-    .then(res => res.json())
-    .then(Img_data => this.setState({ profileImg_data: Img_data }))
+      .then(res => res.json())
+      .then(Img_data => this.setState({ profileImg_data: Img_data }))
 
     axios.get("/users/coursedetails")
-            .then(result => this.setState({ data: result.data.map((data) => { return { value: data._id, label: data.Usercourse } }) }))
-            .then(this.setState({ loading: false }))
-        
+      .then(result => this.setState({ data: result.data.map((data) => { return { value: data._id, label: data.Usercourse } }) }))
+      .then(this.setState({ loading: false }))
+
   }
   onFileChange(e) {
     this.setState({ profileImg: e.target.files[0] })
   }
   onSubmitfile(e) {
     e.preventDefault()
-    this.setState({ dataname:e.target.value })
-    window.open("http://localhost:3001/uploadfile/files/"+""+this.state.dataname+"")
+    this.setState({ dataname: e.target.value })
+    window.open("http://localhost:3001/uploadfile/files/" + "" + this.state.dataname + "")
     // console.log(e.target.value)
     //  fetch('/uploadfile/files/'+ e.target.value)
     //  .then(res => res.json())
@@ -59,9 +59,9 @@ class uploadfile extends Component {
   onSubmit(e) {
     e.preventDefault()
     //const formData = new FormData()
-    var newFileName = this.state.profileImg.name + "_Courseid_"+this.state.selectedOption;
-            var formData = new FormData();
-            formData.append('file', this.state.profileImg, newFileName);
+    var newFileName = this.state.profileImg.name + "_Courseid_" + this.state.selectedOption;
+    var formData = new FormData();
+    formData.append('file', this.state.profileImg, newFileName);
     //formData.append('file', this.state.profileImg)
     fetch("/uploadfile/upload", {
       mode: 'no-cors',
@@ -84,57 +84,57 @@ class uploadfile extends Component {
   handleDeleteEvent = (e) => {
     e.preventDefault();
     fetch('/uploadfile/files/' + e.target.id, {
-        method: 'delete',
-        headers: {
-            'Content-Type': 'application/json'
-        }
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }).then(res => res.json())
-    .then(Img_data => this.setState({ profileImg_data: Img_data }))
-}
+      .then(Img_data => this.setState({ profileImg_data: Img_data }))
+  }
 
   render() {
     const { selectedOption } = '';
-    const view =this.state.profileImg_data.map(data => {
-    return (<div key={data._id}>
-      {/* <img  src={"/uploadfile/image/"+data.filename}  alt="Placeholder image"/> */}
-     <div className="row">
-       <div>
-       <button type="submit" id={data._id} style={{backgroundColor:"#3f51b5", color:"#fff"}}
-       onClick={this.onSubmitfile} value={data.filename}> 
-       {data.filename.substr(0, data.filename.indexOf('_Courseid_'))} </button>
-       </div>
-    <div>
-    <button style={{backgroundColor:"#CD5C5C", color:"#fff"}} type="submit" id={data._id} onClick={this.handleDeleteEvent}> Delete </button>
-    </div>
-    </div>
-    </div>                                                                        
-    )
-  })
-    return (
-      <div style={{ paddingTop: "50px",paddingLeft:"15px"}}>
-                        <div className="row" style={{ background: "#cce6ff", width: "100%" }}>
-                            <div className="container">
-        <h3>
-          Study Material Upload
-			</h3>
-      
-      <div style={{ display: (this.state.visiblebutton ? 'block' : 'none') }}>
-      <div className="col-lg-3 col-xl-3 col-md-3 col-sm-6">
-       <Select value={selectedOption} isDisabled={this.state.loading} classname="form-control input-sm" options={this.state.data} onChange={this.handleChange} placeholder="Course Selection" />
+    const view = this.state.profileImg_data.map(data => {
+      return (<div key={data._id}>
+        {/* <img  src={"/uploadfile/image/"+data.filename}  alt="Placeholder image"/> */}
+        <div className="row">
+          <div>
+            <button type="submit" id={data._id} style={{ backgroundColor: "#3f51b5", color: "#fff" }}
+              onClick={this.onSubmitfile} value={data.filename}>
+              {data.filename.substr(0, data.filename.indexOf('_Courseid_'))} </button>
+          </div>
+          <div style={{ display: (this.state.visiblebutton ? 'block' : 'none') }}>
+            <button style={{ backgroundColor: "#CD5C5C", color: "#fff" }} type="submit" id={data._id} onClick={this.handleDeleteEvent}> Delete </button>
+          </div>
         </div>
-          <input type="file" onChange={this.onFileChange} />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            onClick={this.onSubmit}
-          >
-            Upload
-          </Button></div>
-          {view}
-</div>
       </div>
-    </div>
+      )
+    })
+    return (
+      <div style={{ paddingTop: "50px", paddingLeft: "15px" }}>
+        <div className="row" style={{ background: "#cce6ff", width: "100%" }}>
+          <div className="container">
+            <h3>
+              Study Material Upload
+			</h3>
+
+            <div style={{ display: (this.state.visiblebutton ? 'block' : 'none') }}>
+              <div className="col-lg-3 col-xl-3 col-md-3 col-sm-6">
+                <Select value={selectedOption} isDisabled={this.state.loading} classname="form-control input-sm" options={this.state.data} onChange={this.handleChange} placeholder="Course Selection" />
+              </div>
+              <input type="file" onChange={this.onFileChange} />
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                onClick={this.onSubmit}
+              >
+                Upload
+          </Button></div>
+            {view}
+          </div>
+        </div>
+      </div>
     );
   }
 }
