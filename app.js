fa693badebc599a05898a3c6a5e473712f974389
Vasundhra91 <51117751+Vasundhra91@users.noreply.github.com>
2020-06-01 17:18:35 +0530

    s

--- app.js
+++ app.js
@@ -22,17 +22,17 @@ app.use('/uploaduserphoto', uploadfilePhotoRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+app.use(express.static(__dirname+'/ConfigFile'));
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 controller(app)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('Dashboard/build'));
