d16bb2688dd37e42fb3e4636f32ebe021d0b0dda
Vasundhra91 <51117751+Vasundhra91@users.noreply.github.com>
2020-06-01 18:12:25 +0530

    make it better

--- app.js
+++ app.js
@@ -22,24 +22,25 @@ app.use('/uploaduserphoto', uploadfilePhotoRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-app.use(express.static(__dirname+'/ConfigFile'));
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 controller(app)
+
 if (process.env.NODE_ENV === 'production') {
-  console.log("git push heroku master")
-  app.use(express.static('Dashboard/build'));
+  app.use(express.static('client/build'));
   app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'Dashboard', 'build','index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
    
   });
 }
+app.listen(3001);
 module.exports = app;
